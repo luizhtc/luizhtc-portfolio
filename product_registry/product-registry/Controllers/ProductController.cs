d445cb17560c7536d65b912fc60891e9ed4c4217
liltz <luizhtrincac@gmail.com>
2024-05-08 23:06:50 -0300

    added DAL

--- product_registry/product-registry/Controllers/ProductController.cs
+++ product_registry/product-registry/Controllers/ProductController.cs
@@ -3,52 +3,36 @@ using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using product_registry.Models;
+using product_registry.Repository.DAL;
 
 namespace product_registry;
 
 public class ProductController : Controller
 {
+    private readonly ProductRepository productRepository;
     private readonly ILogger<ProductController> _logger;
 
     public ProductController(ILogger<ProductController> logger)
     {
         _logger = logger;
+
+        productRepository = new ProductRepository();
     }
 
+    [HttpGet]
     public IActionResult Index()
     {
-        ProductModel product = new ProductModel {
-            Id = 1,
-            Name = "Test",
-            Price = 1.5,
-            Description = "TestDesc",
-            RegistryDate = DateTime.Now,
-            Useable = true
-        };
+        var result = productRepository.Read();
 
-        var products = new List<ProductModel>
-        {
-            product
-        };
-
-        return View(products);
+        return View(result);
     }
 
-    public void Create() {
-        using var conn = new SqliteConnection($"Data Source=product.db");
+    [HttpGet]
+    public IActionResult Create()
+    {
+        productRepository.Create();
 
-        const string sql = @"CREATE TABLE IF NOT EXISTS product (
-                                [ID] INT IDENTITY(1,1),
-                                [NAME] NVARCHAR(50) NOT NULL,
-                                [PRICE] DECIMAL(10,2) NOT NULL,
-                                [DESCRIPTION] NVARCHAR(100) NULL,
-                                [REGISTRY_DATE] DATE NOT NULL,
-                                [USEABLE] BIT NOT NULL
-                            );";
-
-        int v = conn.Execute(sql);
-
-        Console.WriteLine(v);
+        return RedirectToAction("Index", "Product");
     }
 
     public IActionResult Register()
@@ -61,14 +45,7 @@ public class ProductController : Controller
     {
         if (ModelState.IsValid)
         {
-            using var conn = new SqliteConnection($"Data Source=product.db");
-
-            const string sql = @"INSERT INTO product (NAME, PRICE, DESCRIPTION, REGISTRY_DATE, USEABLE)
-                                VALUES ('EXAMPLE', 1.5, 'EXAMPLE DESCRIPTION', TO_DATE('2024-05-08'), 1);";
-
-            int v = conn.Execute(sql);
-
-            Console.WriteLine(v);
+            productRepository.Insert(newProduct);
 
             return RedirectToAction("Index", "Product");
         }
@@ -77,7 +54,6 @@ public class ProductController : Controller
             return View(newProduct);
         }
     }
-    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
