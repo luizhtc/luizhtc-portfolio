d31d7f0ca247cb6375b7a9261af701fecb7ef9ba
Luiz Henrique Trinca Caldo <43818123+luizhtc@users.noreply.github.com>
2024-05-16 18:36:00 -0300

    Added product-registry in an unfinished state (#2)
    * base project and edits
    
    * added sqlite
    
    * SQLite Core tests
    
    * Added product registry view
    
    * added understanding of tempdata
    
    * SQL commands and model changes
    
    * commit-080520242059
    
    * added DAL

---
+++ product_registry/product-registry/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace product_registry.Models;
+
+public class ProductModel {
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Product Name is required.")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Product name must have at least 2 characters and maximum 50 characters")]
+    [Display(Name = "Product Name")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Price is required.")]
+    [Display(Name = "Price")]
+    public double Price { get; set; }
+
+    [Display(Name = "Description")]
+    public string Description { get; set; }
+
+    public DateTime RegistryDate { get; set; }
+    
+    [Required(ErrorMessage = "Useable flag is required.")]
+    public bool Useable { get; set; }
+}
