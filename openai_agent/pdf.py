e285015184030a4f8a2fcca302dea05d48f68d03
liltz <luizhtrincac@gmail.com>
2024-05-20 21:45:13 -0300

    added daft punk pdf tool

--- openai_agent/pdf.py
+++ openai_agent/pdf.py
@@ -1,20 +1,29 @@
 import os
 
-from llama_index import (
+from llama_index.core import (
     StorageContext,
     VectorStoreIndex,
     load_index_from_storage
 )
 
-from llama_index.readers import PDFReader
+from llama_index.core import SimpleDirectoryReader
 
-def get_index(data, index_name):
+def get_index(data_path, index_name):
     index = None
     if not os.path.exists(index_name):
         print("building index", index_name)
-        index = VectorStoreIndex.from_documents(data, show_progress=True)
+
+        reader = SimpleDirectoryReader(
+            input_files=[data_path]
+        )
+
+        docs = reader.load_data()
+        print(f"Loaded {len(docs)} docs")
+
+        index = VectorStoreIndex.from_documents(docs, show_progress=True)
         index.storage_context.persist(persist_dir=index_name)
     else:
+        print("index already exists", index_name)
         index = load_index_from_storage(
             StorageContext.from_defaults(persist_dir=index_name)
         )
@@ -23,7 +32,6 @@ def get_index(data, index_name):
 
 
 pdf_path = os.path.join(".data","Daft Punk.pdf")
-daft_pdf = PDFReader().load_data(file=pdf_path)
 
-daft_index = get_index(daft_pdf, "daft_punk")
+daft_index = get_index(pdf_path, "daft_punk")
 daft_engine = daft_index.as_query_engine()
